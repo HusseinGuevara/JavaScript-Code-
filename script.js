ef4ac86b81ba8881cf2ecd1983c845d31b4e7d4e
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2022-03-24 21:04:18 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,94 +1,138 @@
 'use strict';
 
-// function calcAge(birthYear) {
-//     const age = 2022 - birthYear;
 
-//     function printAge() {
-//         const output = `${firstName}, You are ${age}. You were born in ${birthYear}.`;
-//         console.log(output);
+const bookings = [];
+const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers) {
+    // ES5 Default Values
+    // numPassengers = numPassengers || 1; 
+    // price = price || 199;
 
-//         if(birthYear >= 1981 && birthYear <= 1996) {
-//             var millenial = true;
-//             const str = `Oh, ${firstName}, you're a millenial.`;
-//             console.log(str);
-
-//             function add(a, b) {
-//                 return a + b;
-//             }
-//         }
-//         // console.log(add(158, 6)); Functions are blocked scoped in strict mode. The funstion will remain undefined 
-//         // if called while in strict mode.
-        
-//         console.log(millenial);
-//     }
-//     printAge();
-//     console.log(firstName);
-//     return age;
-// }
-
-// const firstName = "Huss";
-// calcAge(1990);
-
-
-// // Variable TDZ
-// // console.log(job); // var will be undeifned if they are being hoisted
-// // console.log(race); // let and const will be uninitialized if they are hoisted 
-// // console.log(year);
-
-
-// var job = "teacher";
-// let race = "Mexican";
-// const year = 1990;
-
-
-// // Function TDZ
-
-// console.log(addDeco(5, 5));
-// console.log(addNums(10, 5));
-// console.log(addDeci(15, 5));
-
-// function addDeco(a, b) {
-//     return a + b;
-// }
-
-// const addNums = function(a, b) {
-//     return a + b;
-// }
-
-// const addDeci = (a, b) => a + b;
-
-// Primitive and Reference Types (Primitive vs Objects)
-
-// Primitive Type
-// let lastName = "Guevara";
-// let newLastName = lastName;
-// lastName = "Zarate";
-// console.log(lastName, newLastName);
-
-
-// Reeference Type
-// const jazmin = {
-//     lastName : "Lopez",
-//     age : 31
-// }
-
-// const marriedJazmin = jazmin;
-// marriedJazmin.lastName = "Guevara";
-// console.log("Old Jazmin:", jazmin);
-// console.log("New Jazmin:", marriedJazmin);
-
-// Copying Objects
-
-const jazmin2 = {
-    firstName : "Jazmin",
-    lastName : "Lopez",
-    age : 31,
-    family : ["John, Jessica"],
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
 };
 
-const jazminCopy = Object.assign({}, jazmin2);
-jazminCopy.lastName = "Guevara";
-jazminCopy.family.push("Damien");
-jazminCopy.family.push("Mike");
-console.log("Old Jazmin:", jazmin2);
-console.log("New Jazmin:", jazminCopy);
+// createBooking("AC122");
+// createBooking("AC122", 2, 300);
+// createBooking("AC122", 10);
+// createBooking("AC122", 5);
+// createBooking("AC122", undefined, 300);
+
+// Arguments
+
+const flight = "LH234";
+const hussein = {
+    name: "Hussein Guevara",
+    passport: 985656541
+};
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = "LH999";
+    passenger.name = "Mr." + passenger.name;
+
+    if(passenger.passport === 985656541) {
+        alert("Checked In!");
+    } else {
+        alert("Wrong Passport!");
+    }
+};
+
+// checkIn(flight, hussein);
+// console.log(hussein);
+// console.log(flight);
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() * 1000000000)
+};
+
+// newPassport(hussein);
+// checkIn(flight, hussein);
+
+// Accepting Callback Functions
+
+const oneWord = function(str) {
+    return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(" ");
+    return [first.toUpperCase(), ...others].join(" ");
+};
+
+// Higher-Order Function
+const transformer = function(str, fn) {
+    // console.log(`Original String: ${str}`);
+    // console.log(`Transformed string: ${fn(str)}`);
+    // console.log(`Called by: ${fn.name}`);
+}
+
+transformer("JavaScript is the best!", upperFirstWord);
+
+const high5 = function() {
+    // console.log("Hello!")
+}
+document.body.addEventListener("click", high5);
+
+// Returning Functions
+
+const greet = function(greeting) {
+    return function(name) {
+        console.log(`${greeting} ${name}`);
+    }
+}
+
+const greeterHey = greet("Hey");
+// greeterHey("Huss");
+// greeterHey("Jazz");
+
+// Call and Apply Methods
+
+const alaska = {
+    airline: "AlasKa",
+    iateCode: "Ak",
+    bookings: [],
+    booK(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iateCode}${flightNum}`);
+        this.bookings.push({fligth: `${this.iateCode}${flightNum}`, name});
+    }
+}
+
+alaska.booK(289, "Hussein");
+// console.log(alaska);
+
+const southWest = {
+    airline: "SouthWest",
+    iateCode: "SW",
+    bookings: [],
+}
+
+const book = alaska.booK;
+
+// The this keyword only wokrs in the object's scope; Does not work to call book method;
+// book(547, "Hussein");
+
+// Call
+book.call(alaska, 474, "Hussein"); // Use call() this target the object you want 
+console.log(alaska);
+
+book.call(southWest, 666, "Jaz");
+console.log(southWest);
+
+const swiss = {
+    airline: "Swiss Air Lines",
+    iateCode: "SAL",
+    bookings: []
+}
+
+book.call(swiss, 777, "Jose");
+console.log(swiss);
+
+// Apply Method
+
+const flightData = [781, "John Cena"]; // apply() arguments are where the this keywaord is pointing and an array
+book.apply(swiss, flightData);
+console.log(swiss);
